84389edb2b21f78798b8da3d893e9ffc61cb64e7
rickypang <64291268+rickypang@users.noreply.github.com>
2020-04-24 14:12:41 -1000

    personal

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -11,9 +11,9 @@ function init() {
   // these URLs come from Google Sheets 'shareable link' form
   // the first is the polygon layer and the second the points
   var polyURL =
-    "https://docs.google.com/spreadsheets/d/1EUFSaqi30b6oefK0YWWNDDOzwmCTTXlXkFHAc2QrUxM/edit?usp=sharing";
+    "https://docs.google.com/spreadsheets/d/1K_gQ9td2QvHMp49GZDLSEQsA0EPYjvqZn3IS1BaLgjA/edit?usp=sharing";
   var pointsURL =
-    "https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing";
+    "https://docs.google.com/spreadsheets/d/1WuE3ceHTVKlD8BYaTx71nZFP1JihkD6ra9aQ2x0Nxyk/edit?usp=sharing";
 
   Tabletop.init({ key: polyURL, callback: addPolygons, simpleSheet: true });
   Tabletop.init({ key: pointsURL, callback: addPoints, simpleSheet: true }); // simpleSheet assumes there is only one table and automatically sends its data
